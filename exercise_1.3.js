9823a9e6748a7b1762d35ccdd4f0026b2f61072f
David Delgado <ddelgad5@hawk.iit.edu>
2018-05-23 21:53:19 -0500

    Add test cases.

--- exercise_1.3.js
+++ exercise_1.3.js
@@ -6,3 +6,11 @@ const canVote = function( age ) {
  else if (age > 17) return "yay, start voting";
  else return "please vote";
 };
+
+//testing
+console.log(canVote(12));      //no, can't vote
+console.log(canVote("12"));    //no, can't vote
+console.log(canVote(17));      //no, can't vote
+console.log(canVote('@18'));   //no, can't vote
+console.log(canVote(18));      //yay, start voting
+console.log(canVote(28));      //please vote
